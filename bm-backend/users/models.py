6808a58ac06682cb8cb194ff74c1d87bafd064b3
arena <sand@sand.com>
2021-10-03 15:30:16 -0700

    added additional registration fields

--- bm-backend/users/models.py
+++ bm-backend/users/models.py
@@ -2,5 +2,19 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
+
+    # max username length 32
+    username = models.CharField(32, unique=True)
+    email = models.EmailField(unique=True)
+    
+    # these 5 are not required
+    avatar = models.TextField(null=True, blank=True)
+    pronouns = models.IntegerField(null=True, blank=True)
+    referrer = models.CharField(32, null=True, blank=True)
+    gender = models.IntegerField(null=True, blank=True)
+    experience = models.IntegerField(null=True, blank=True)
+    
+    captchaToken = models.TextField()
+
     def __str__(self):
         return self.username
--- bm-backend/users/serializers.py
+++ bm-backend/users/serializers.py
@@ -5,4 +5,4 @@ from .models import CustomUser
 class UserSerializer(ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'last_login', 'date_joined', 'is_staff')
+        fields = ('username', 'email', 'avatar', 'pronouns', 'referrer', 'gender', 'experience')
