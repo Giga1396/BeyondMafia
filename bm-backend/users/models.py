5b85f3adb18680958b346d5526b453de08d8ab67
arena <sand@sand.com>
2021-10-06 14:21:17 -0700

    migrations for extra fields

--- bm-backend/users/models.py
+++ bm-backend/users/models.py
@@ -4,16 +4,16 @@ from django.db import models
 class CustomUser(AbstractUser):
 
     # max username length 32
-    username = models.CharField(32, unique=True)
+    username = models.CharField(max_length=32, unique=True)
     email = models.EmailField(unique=True)
     
     # these 5 are not required
     avatar = models.TextField(null=True, blank=True)
     pronouns = models.IntegerField(null=True, blank=True)
-    referrer = models.CharField(32, null=True, blank=True)
+    referrer = models.CharField(max_length=32, null=True, blank=True)
     gender = models.IntegerField(null=True, blank=True)
     experience = models.IntegerField(null=True, blank=True)
-    
+
     captchaToken = models.TextField()
 
     def __str__(self):
